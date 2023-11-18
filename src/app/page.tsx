57c1c7f9f3d379bdfb993b26a71cb7dff4395bf2
Jose Garfias <garfiaslopez@hotmail.com>
2023-11-18 16:03:48 +0100

    Add register form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <WelcomeBannerComponent />
       <iframe
           width="100%"
-          src="https://forms.office.com/Pages/ResponsePage.aspx?id=wPKBgTgnB0iSOv3jtFLHFZ6seHNzg6pOi2aeKwqdEG9UQ0Q1UzcxWE5HSzFRQk5GNFhTRlAxT1lUMC4u&embed=true"
+          src="https://forms.office.com/Pages/ResponsePage.aspx?id=wPKBgTgnB0iSOv3jtFLHFZ6seHNzg6pOi2aeKwqdEG9UNDRIUUJEOE0wREVOVFFXMEdQV1RVVUhRMy4u&embed=true"
           frameBorder="0"
           style={{ minHeight: '100vh' }}
       />
